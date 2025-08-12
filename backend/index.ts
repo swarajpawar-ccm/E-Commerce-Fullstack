f7294b9922324c24d47a462d72b8e6e0b52062df
swarajpawar-ccm <Swaraj.Pawar_EXT@myccmortgage.com>
2025-08-12 12:13:27 +0530

    Updated backed

--- backend/index.ts
+++ backend/index.ts
@@ -9,8 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
-
+app.use(cors()); 
 app.use('/api/products',productRoutes)
 
 mongoose
